42de288ca50bfc936fd509f0fd3199d3d4f54756
unknown <oskaras2011@yandex.com>
2019-04-16 15:40:00 +0300

    task 2 fully corrected

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>As valdau HTML! <?php print("ir PHP");?></title>
-        <meta charset="UTF-8">
+    <title>As valdau HTML! <?php print("ir PHP!");?></title>
+    <meta charset="UTF-8">
 </head>
 <body>
-    <h1>Oskaras</h1><?php print("HTML ir PHP asas") ?>
-    <p>Moku kurti statini turini</p>
-    <?php
-    print(date("Y-m-d H:i:s"));
-    ?>
+    <h1>Oskaras - HTML <?php print("ir PHP"); ?> asas!</h1>
+    <p>Moku kurti statini turini <?php print(date("Y-m-d H:i:s"));?></p>
 </body>
 </html>
