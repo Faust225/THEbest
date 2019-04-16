bbdad7ceef6b8185930a0e56512412cc4cd19a38
unknown <oskaras2011@yandex.com>
2019-04-16 15:11:00 +0300

    corrected

--- index.php
+++ index.php
@@ -2,11 +2,9 @@
 <html>
 <head>
 	<title>As valdau HTML!</title>
+        <meta charset="UTF-8">
 </head>
 <body>
-	<?php
-	?>
-    
     <h1>Oskaras</h1><!--HTML asas-->
     <p>Moku kurti statini turini</p>
 </body>
