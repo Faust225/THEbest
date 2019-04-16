9bd506ac3f25880aa3455d085d81992b9e40ae0b
unknown <oskaras2011@yandex.com>
2019-04-16 15:25:17 +0300

    date corrected

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>As valdau HTML!</title>
+	<title>As valdau HTML! <?php print("ir PHP");?></title>
         <meta charset="UTF-8">
 </head>
 <body>
-    <h1>Oskaras</h1><!--HTML asas-->
+    <h1>Oskaras</h1><?php print("HTML ir PHP asas") ?>
     <p>Moku kurti statini turini</p>
     <?php
-    print("<h1>ir PHP</h1>");
-    print("<p>". date("Y-m-d H:i:s") . "</p>")
+    print(date("Y-m-d H:i:s"));
     ?>
 </body>
 </html>
