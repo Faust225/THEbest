de69cd6839d0a7d8d0be898e9dcb1f3b58b4ec03
unknown <oskaras2011@yandex.com>
2019-04-16 15:46:38 +0300

    clean code with cltr + shift + f

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>As valdau HTML! <?php print("ir PHP!");?></title>
-    <meta charset="UTF-8">
-</head>
-<body>
-    <h1>Oskaras - HTML <?php print("ir PHP"); ?> asas!</h1>
-    <p>Moku kurti statini turini <?php print(date("Y-m-d H:i:s"));?></p>
-</body>
+    <head>
+        <title>As valdau HTML! <?php print("ir PHP!"); ?></title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Oskaras - HTML
+            <?php print("ir PHP"); ?>
+            asas!
+        </h1>
+        <p>
+            Moku kurti statini turini 
+            <?php print(date("Y-m-d H:i:s")); ?>
+        </p>
+    </body>
 </html>
