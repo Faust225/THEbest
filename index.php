33d52e71b4ebd52a32abeee337cd01eb7323c602
unknown <oskaras2011@yandex.com>
2019-04-16 14:55:02 +0300

    uzduotys padarytos

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>As valdau HTML!</title>
 </head>
 <body>
 	<?php
-	echo "Hello world";
+	echo "Hello darkness my old friend";
+        
+        function add($a, $b) {
+            return $a + $b;
+        }
+       echo date('H');
+        
+       echo "<br>" . add(5, 8);
 	?>
+    
+    <h1>Oskaras</h1><!--HTML asas-->
+    <p>Moku kurti statini turini</p>
 </body>
 </html>
