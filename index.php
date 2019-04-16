74d5c17287159fd0d0fcd313c6d00548ea41dd0f
unknown <oskaras2011@yandex.com>
2019-04-16 15:00:54 +0300

    php task done

--- index.php
+++ index.php
@@ -5,14 +5,6 @@
 </head>
 <body>
 	<?php
-	echo "Hello darkness my old friend";
-        
-        function add($a, $b) {
-            return $a + $b;
-        }
-       echo date('H');
-        
-       echo "<br>" . add(5, 8);
 	?>
     
     <h1>Oskaras</h1><!--HTML asas-->
