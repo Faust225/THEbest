0caac0a1efb969ce7f481192a50e908e7d540977
unknown <oskaras2011@yandex.com>
2019-04-17 11:56:52 +0300

    git test

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php // Komentarai
-        date_default_timezone_set('Europe/Vilnius');
+        <title><?php
+            // Komentarai
+            date_default_timezone_set('Europe/Vilnius');
 
-        print 'PHP lydes ir ' . date( mt_rand(2000, 2029), date("Y"));
-         ?>
+            print 'PHP lydes ir ' . date(mt_rand(2000, 2029), date("Y"));
+            ?>
         </title>
         <meta charset="UTF-8">
     </head>
-    <body style="background: rgb<?php print  '('. (rand(0, 255)) . ',' . (rand(0, 255)) . ',' . (rand(0, 255)). ')' . ';'; ?>">
-        <h1 style="font-size: <?php print rand(16, 30) . 'px;'; ?>">
+    <body style="background: rgb<?php print '(' . (mt_rand(0, 255)) . ',' . (mt_rand(0, 255)) . ',' . (mt_rand(0, 255)) . ')' . ';'; ?>">
+        <h1 style="font-size: <?php print mt_rand(16, 30) . 'px;'; ?>">
             Oskaras - galbut turesiu
             <?php print " " . rand(1, 5) . ' vaiku'; ?>
         </h1>
-        <p style="color: <?php print '#' . rand(100, 999) . ';'; ?>">
+        <p style="color: <?php print '#' . mt_rand(100, 999) . ';'; ?>">
             D. Trum'as nebebus prezidentu 
-            <?php
-                print date( mt_rand(2021, 2031), date("Y")) . '-' . date("m-d");  ?>
+            <?php print date(mt_rand(2021, 2031), date("Y")) . '-' . date("m-d"); ?>
             <?php // As toks vienisas jaunas paragrafas... ?>
         </p>
         <p>
             <?php // Mindaugo svetimo kodo gatves... ?>
         </p>
-        <img src="oneToSix/<?php print mt_rand(1, 6);?>.png"><br>
+
+        <!-- Task 9 -->
+        <img src="oneToSix/<?php print mt_rand(1, 6); ?>.png"><br>
+        
         <?php
         
-        $imgWidth = date("s")+ 300;
-        $imgHeight = date("s") + 250;
-        
-        if(idate("s") != 0){
+        // task 10 and 11
+        $imgWidth = 5 + date("s") + 300;
+        $imgHeight = 5 + date("s") + 250;
+
+        if (idate("s") != 0) {
             print "<img src=\"bomb.jpg\" width=\" $imgWidth \" height=\" $imgHeight \">";
-        }
-        else {
-             print "<img src=\" explosion.jpg \">";
+        } else {
+            print "<img src=\" explosion.jpg \">";
         }
         ?>
         <br>
-        <?php print date("s");?>
+        <?php print date("s"); ?>
     </body>
 </html><!-- alt + shift + f   oskarShop.lt-->
