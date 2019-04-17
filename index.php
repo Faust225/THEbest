c0cb4fa656ad065c925f8199c5ed5c88cce20107
unknown <oskaras2011@yandex.com>
2019-04-17 18:56:32 +0300

    Trash counter per days

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ function reached() {
 
     $limit = ($max_kaupo_turis + $siuskles_turis); // max limit 
     $after_days = floor(($max_kaupo_turis + $siuskles_turis) / $siuskliu_turis_avg_per_day); // number of days to throw trash
-    $thrash_per_days = $after_days * $siuskliu_turis_avg_per_day; // thresh count after days
+    $thrash_per_days = $after_days * $siuskliu_turis_avg_per_day; // thresh count
     $space_left = $limit - $thrash_per_days; // free space left
 
-    if ($space_left > 0) {
+    if ($space_left > 2) {
         print "Po $after_days d. prisirinko $thrash_per_days of $limit ";
     } else {
         print"Po $after_days dienu " . date("Y-m-d", strtotime("+ $after_days day"))
