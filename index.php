36dd2652e0a4bc7b7c4d6cc8338fed625b816d47
unknown <oskaras2011@yandex.com>
2019-04-17 16:48:53 +0300

    clean code

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    $taken = rand(1, 100);
+    $return_one = rand(101, 200);
+    $return_two = rand(301, 400);
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,12 +10,9 @@
         <meta charset="UTF-8">
     </head>
     <body>
-
-        <?php $imgLoc = "images/explosion.jpg"; ?>
-
-        <img src="<?php print $imgLoc ?>">
-        <img src="<?php print $imgLoc ?>">
-        <img src="<?php print $imgLoc ?>">
-        <img src="<?php print $imgLoc ?>">
+        <h1>Skolos skaiciuokle</h1>
+        <h3>Jei paemei <?php print $taken; ?>EU</h3>
+        <h3>Su dviem kabanciais grazinsi <?php print $return_two; ?></h3>
+        <h3>Su vienu kabanciu grazinsi <?php $return_one; ?></h3>
     </body>
 </html><!-- alt + shift + f   oskarShop.lt-->
