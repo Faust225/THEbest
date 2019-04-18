88458ae8f72571a03c213ad08f628ed010266f35
unknown <oskaras2011@yandex.com>
2019-04-18 15:40:55 +0300

    Fuel used l / km and money gave

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $distance = rand(1, 100);
 $consumption = 7.5; // for 100 km
 $price = 1.3; // EU for L
+$money_in_hands = 100;
 
 $used_fuel = ($distance * $consumption) / 100;
 $trip_price = $used_fuel * $price;
@@ -15,6 +16,13 @@ $trip_price = $used_fuel * $price;
     <body>
         <?php print 'Nuvaziavus ' . $distance . ' km ' .
                 'masina sunaudos ' . round($used_fuel, 2) . ' l kuro ' .
-                'kaina: ' . round($trip_price, 2) . ' EU';?>
+                'kaina: ' . round($trip_price, 2) . ' EU';
+        if($money_in_hands >= $trip_price) {
+            print ' enought money for trip';
+        }
+        else {
+            print ' not enought money';
+        }
+?>
     </body>
 </html><!-- alt + shift + f   oskarShop.lt-->
