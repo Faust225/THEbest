ef2f2402c14ebb1974e470e24502a87b7e24bc81
unknown <oskaras2011@yandex.com>
2019-04-18 15:14:06 +0300

    Fuel used l / km

--- index.php
+++ index.php
@@ -1,31 +1,20 @@
 <?php
+$distance = rand(1, 100);
+$consumption = 7.5; // for 100 km
+$price = 1.3; // EU for L
 
-function reached() {
-    $siuskles_turis = 40;
-    $siuskliu_turis_avg_per_day = 15;
-    $max_kaupo_turis = rand(1, 10);
-
-    $limit = ($max_kaupo_turis + $siuskles_turis); // max limit 
-    $after_days = floor(($max_kaupo_turis + $siuskles_turis) / $siuskliu_turis_avg_per_day); // number of days to throw trash
-    $thrash_per_days = $after_days * $siuskliu_turis_avg_per_day; // thresh count
-    $space_left = $limit - $thrash_per_days; // free space left
-
-    if ($space_left > 2) {
-        print "Po $after_days d. prisirinko $thrash_per_days of $limit ";
-    } else {
-        print"Po $after_days dienu " . date("Y-m-d", strtotime("+ $after_days day"))
-                . " pirk geliu ir sampano, jeigu nori, kad zmona siuksles pati isnestu"
-                . ", nes prisirinko $thrash_per_days of $limit";
-    }
-}
+$used_fuel = ($distance * $consumption) / 100;
+$trip_price = $used_fuel * $price;
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Siuksles</title>
+        <title>Condition</title>
         <meta charset="UTF-8">
     </head>
     <body>
-        <?php reached(); ?>
+        <?php print 'Nuvaziavus ' . $distance . ' km ' .
+                'masina sunaudos ' . round($used_fuel, 2) . ' l kuro ' .
+                'kaina: ' . round($trip_price, 2) . ' EU';?>
     </body>
 </html><!-- alt + shift + f   oskarShop.lt-->
