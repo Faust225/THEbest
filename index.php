a9fd6a04df6f2e485c207759fb9996152dcbc039
unknown <oskaras2011@yandex.com>
2019-04-19 14:46:54 +0300

    BG image

--- index.php
+++ index.php
@@ -7,13 +7,18 @@
         <title>Condition</title>
         <meta charset="UTF-8">
         <style>
-
+            html {
+                background-image: url(https://i.pinimg.com/originals/52/96/7e/52967e9aee1c617cb0668d3ef1704eba.jpg);
+                position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
            
         </style>
     </head>
     
     <body>
-        
-        awdawd
+        <!-- THEbest start Oskaras index.php -->
+        <div class="all_index">dawdwa</div>
     </body>
 </html><!-- alt + shift + f   oskarShop.lt-->
